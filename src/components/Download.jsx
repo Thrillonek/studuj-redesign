7faad1776eeb19848bf014ddbe40c8b9391cd7d1
Thrillonek <jindrich.kraina@gmail.com>
2024-10-27 15:38:40 +0100

    Download search bar color changed

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -19,14 +19,14 @@ export default function Download({ files, state, setState }) {
 	}
 
 	return (
-		<div className={'top-1/2 left-1/2 z-10 shadow-2xl w-[min(30rem,90%)] duration-300 font-semibold fixed border-2 border-gray-300 bg-[--bg] p-6 rounded-lg -translate-x-1/2 transition-[top] download -translate-y-1/2 ' + (!state && 'top-[150%]')}>
+		<div className={'top-1/2 left-1/2 z-10 shadow-xl w-[min(30rem,90%)] duration-300 font-semibold fixed border-2 border-gray-300 bg-[--bg] p-6 rounded-lg -translate-x-1/2 transition-[top] download -translate-y-1/2 ' + (!state && 'top-[150%]')}>
 			<i onClick={(e) => setState(false)} className='top-2 right-4 absolute text-xl cursor-pointer fa-solid fa-xmark'></i>
 			<h2 className='mb-4 font-bold text-2xl'>Soubory</h2>
 			<form className='flex flex-col mb-2 w-[calc(100%-1rem)]'>
 				<label className='mb-1 text-sm' htmlFor='findFile'>
 					Najít soubor:
 				</label>
-				<input onChange={(e) => setFilter(e.target.value)} className='shadow ml-1 px-2 py-1 border rounded-lg outline-none' id='findFile' type='text' />
+				<input onChange={(e) => setFilter(e.target.value)} className='bg-[--hover] ml-1 px-2 py-1 rounded-lg outline-none' id='findFile' type='text' />
 			</form>
 			<div className='flex flex-col h-[75vh] md:h-[70vh] overflow-auto'>
 				{files.map((file) => {
