67d147d7006ecc57099969b0b605b9617bcbc94a
Thrillonek <jindrich.kraina@gmail.com>
2024-10-27 10:16:59 +0100

    Custom scrollbar

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -17,7 +17,7 @@ export default function Download({ files, state, setState }) {
 	}
 
 	return (
-		<div className={'top-1/2 left-1/2 z-10 shadow-2xl w-[min(30rem,90%)] duration-300 font-semibold fixed border-2 border-gray-300 bg-[--bg] p-6 rounded -translate-x-1/2 transition-[top] download -translate-y-1/2 ' + (!state && 'top-[150%]')}>
+		<div className={'top-1/2 left-1/2 z-10 shadow-2xl w-[min(30rem,90%)] duration-300 font-semibold fixed border-2 border-gray-300 bg-[--bg] p-6 rounded-lg -translate-x-1/2 transition-[top] download -translate-y-1/2 ' + (!state && 'top-[150%]')}>
 			<i onClick={(e) => setState(false)} className='top-2 right-4 absolute text-xl cursor-pointer fa-solid fa-xmark'></i>
 			<h2 className='mb-4 font-bold text-2xl'>Soubory</h2>
 			<form className='flex flex-col mb-2 w-[calc(100%-1rem)]'>
